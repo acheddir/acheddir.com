5fbf26ff4744491449f52bace871e203581e86b5
Abderrahman Cheddir <acheddir@outlook.fr>
2025-11-22 01:17:41 +0100

    chore: remove temp avatar on home page

--- app/[locale]/(site)/page.tsx
+++ app/[locale]/(site)/page.tsx
@@ -1,24 +1,23 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import Image from "next/image";
-import Link from "next/link";
-import { pages, posts } from "#site/content";
-import { ArrowLeft, ArrowRight } from "lucide-react";
-
-import { siteMetadata, defaultAuthor } from "@/lib/metadata";
-import { sortByDate } from "@/lib/utils";
-import { HeroSimple } from "@/components/hero-simple";
-import { Sidebar } from "@/components/home-sidebar";
-import { Mdx } from "@/components/mdx";
-import NewsletterSubscribe from "@/components/newsletter-subscribe";
-import PostPreview from "@/components/post-preview";
-import { useTranslation } from "@/app/i18n";
-import i18next from "@/app/i18n/i18next";
-
+import Image from 'next/image';
+import Link from 'next/link';
 import AvatarHome from '@/public/avatar-home.png';
-import { withLineBreaks } from "@/lib/tsx-utils";
+import { pages, posts } from '#site/content';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
+
+import { defaultAuthor, siteMetadata } from '@/lib/metadata';
+import { withLineBreaks } from '@/lib/tsx-utils';
+import { sortByDate } from '@/lib/utils';
+import { HeroSimple } from '@/components/hero-simple';
+import { Sidebar } from '@/components/home-sidebar';
+import { Mdx } from '@/components/mdx';
+import NewsletterSubscribe from '@/components/newsletter-subscribe';
+import PostPreview from '@/components/post-preview';
+import { useTranslation } from '@/app/i18n';
+import i18next from '@/app/i18n/i18next';
 
 async function getAboutPage(locale: string) {
-  const aboutPage = pages.find((page) => page.slug === "about" && page.locale === locale);
+  const aboutPage = pages.find((page) => page.slug === 'about' && page.locale === locale);
 
   if (!aboutPage) {
     null;
@@ -30,21 +29,21 @@ async function getAboutPage(locale: string) {
 export default async function Home(props: { params: Promise<{ locale: string }> }) {
   const { locale } = await props.params;
 
-  const { t: tc } = await useTranslation(locale, "common");
-  const { t: th } = await useTranslation(locale, "home");
+  const { t: tc } = await useTranslation(locale, 'common');
+  const { t: th } = await useTranslation(locale, 'home');
 
-  const isRtl = i18next.dir(locale) === "rtl";
+  const isRtl = i18next.dir(locale) === 'rtl';
 
   const aboutPage = await getAboutPage(locale);
 
   const localizedPosts = posts
-    .filter((post) => post.status === "published" && post.locale === locale)
+    .filter((post) => post.status === 'published' && post.locale === locale)
     .sort(sortByDate)
     .slice(0, siteMetadata.postsOnHomePage);
 
   return (
     <div className="pb-10">
-      <HeroSimple locale={locale} title={th("hero.title")} subtitle={th("hero.subtitle")} />
+      <HeroSimple locale={locale} title={th('hero.title')} subtitle={th('hero.subtitle')} />
       <div className="container mt-12 max-w-6xl">
         <div className="grid grid-cols-1 place-items-start justify-between gap-12 lg:grid-cols-3">
           <div className="col-span-1 w-full lg:col-span-2">
@@ -57,7 +56,7 @@ export default async function Home(props: { params: Promise<{ locale: string }> 
               href={`/${locale}/posts`}
               className="mt-10 flex items-center py-2 text-sm text-accent-foreground underline-offset-4 hover:text-muted-foreground hover:underline"
             >
-              {th("posts.seeall")}{" "}
+              {th('posts.seeall')}{' '}
               {isRtl ? <ArrowLeft className="ml-2 h-4 w-4" /> : <ArrowRight className="mr-2 h-4 w-4" />}
             </Link>
           </div>
@@ -75,21 +74,17 @@ export default async function Home(props: { params: Promise<{ locale: string }> 
       )}
       {aboutPage && (
         <div className="container max-w-6xl">
-          <h2 className="mb-8 font-heading text-4xl font-bold">{th("about.whois")}</h2>
+          <h2 className="mb-8 font-heading text-4xl font-bold">{th('about.whois')}</h2>
           <div className="grid grid-cols-1 place-items-start justify-between gap-12 lg:grid-cols-3">
             <div className="col-span-1 mx-auto flex flex-col items-center justify-center">
-              <Image
-                src={AvatarHome}
-                alt={defaultAuthor.name}
-                width={400}
-                height={498}
-                className="h-auto w-72 -rotate-1 hover:rotate-3"
-              />
+              {/* TODO: put avatar here */}
               <div className="text-center">
                 <h1 className="font-heading text-2xl font-bold">{tc(defaultAuthor.name)}</h1>
                 <p className="text-muted-foreground">{defaultAuthor.jobTitle}</p>
                 <p dir="ltr" className="text-muted-foreground">
-                  <Link className="underline text-red-800" href={defaultAuthor.company?.website}>{defaultAuthor.company?.name}</Link>
+                  <Link className="text-red-800 underline" href={defaultAuthor.company?.website}>
+                    {defaultAuthor.company?.name}
+                  </Link>
                 </p>
               </div>
             </div>
